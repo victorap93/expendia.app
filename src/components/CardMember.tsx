780beffcc177697c17377ac09f1a826cfe13a326
luigi-raynel-dev <devluigiraynel@gmail.com>
2023-07-06 22:10:02 -0300

    feat: fetch user at members list

--- src/components/CardMember.tsx
+++ src/components/CardMember.tsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import CardBox from './CardBox'
 import { HStack, Text, VStack } from 'native-base'
 import { UserProps } from '../context/AuthContext'
 import { MemberAvatar } from './MemberAvatar'
+import { api } from '../lib/axios'
 
 interface CardMemberProps {
   member: UserProps
@@ -10,23 +11,43 @@ interface CardMemberProps {
 }
 
 export default function CardMember({ member, fetchUser }: CardMemberProps) {
-  return (
+  const [user, setUser] = useState<UserProps | undefined>()
+
+  async function getUser() {
+    try {
+      const response = await api.get(`/user/${member.email}`)
+      if (response.status !== 404) setUser(response.data.user || member)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  useEffect(() => {
+    setUser({ ...member })
+    if (fetchUser) getUser()
+
+    return () => setUser(undefined)
+  }, [member])
+
+  return user ? (
     <CardBox p={3}>
       <HStack alignItems="center" space={3}>
-        <MemberAvatar member={member} size="sm" />
+        <MemberAvatar member={user} size="sm" />
         <VStack>
           <Text color="white" fontSize="md">
-            {member.firstname
-              ? `${member.firstname} ${member.lastname || ''}`
-              : member.email}
+            {user.firstname
+              ? `${user.firstname} ${user.lastname || ''}`
+              : user.email}
           </Text>
-          {member.firstname && (
+          {user.firstname && (
             <Text color="gray.200" fontSize="sm">
-              {member.email}
+              {user.email}
             </Text>
           )}
         </VStack>
       </HStack>
     </CardBox>
+  ) : (
+    <></>
   )
 }
