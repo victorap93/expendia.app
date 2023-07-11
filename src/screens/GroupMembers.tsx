8c767665af20d9321f023878a918a1a0b4de5932
luigi-raynel-dev <devluigiraynel@gmail.com>
2023-07-11 08:05:03 -0300

    fix: add scroll view at group members screen

--- src/screens/GroupMembers.tsx
+++ src/screens/GroupMembers.tsx
@@ -93,144 +93,150 @@ export default function GroupMembers() {
   }
 
   return (
-    <Formik
-      initialValues={{
-        title,
-        members,
-        email: ''
-      }}
-      validationSchema={Yup.object({
-        members: Yup.array(
-          Yup.string().email('Digite apenas e-mails válidos')
-        ).required('Erro: Array not defined.'),
-        email: Yup.string().email('E-mail inválido.')
-      })}
-      onSubmit={(values, { setSubmitting }) => submit(values, setSubmitting)}
-    >
-      {({
-        handleChange,
-        handleBlur,
-        handleSubmit,
-        values,
-        errors,
-        touched,
-        isSubmitting,
-        setFieldValue
-      }) => (
-        <VStack
-          flex={1}
-          space={2}
-          minH="full"
-          px={4}
-          py={8}
-          justifyContent="space-between"
-        >
-          <VStack space={8}>
-            <VStack>
-              <Box my={3}>
-                <BackButton />
-              </Box>
-              <Text fontSize={28} color="white">
-                Gostaria de já adicionar alguém no seu grupo?
-              </Text>
-            </VStack>
-            <VStack>
-              <HStack space={2}>
-                <Box width="4/5">
-                  <TextField
-                    error={
-                      touched.email && errors.email ? errors.email : undefined
-                    }
-                    hideMessageError
-                    onChangeText={handleChange('email')}
-                    onBlur={handleBlur('email')}
-                    placeholder="E-mail do membro"
-                    value={values.email || ''}
-                  />
+    <ScrollView>
+      <Formik
+        initialValues={{
+          title,
+          members,
+          email: ''
+        }}
+        validationSchema={Yup.object({
+          members: Yup.array(
+            Yup.string().email('Digite apenas e-mails válidos')
+          ).required('Erro: Array not defined.'),
+          email: Yup.string().email('E-mail inválido.')
+        })}
+        onSubmit={(values, { setSubmitting }) => submit(values, setSubmitting)}
+      >
+        {({
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          values,
+          errors,
+          touched,
+          isSubmitting,
+          setFieldValue
+        }) => (
+          <VStack
+            flex={1}
+            space={2}
+            px={4}
+            py={8}
+            justifyContent="space-between"
+          >
+            <VStack space={8}>
+              <VStack>
+                <Box my={3}>
+                  <BackButton />
                 </Box>
-                <Button
-                  disabled={isDisabled(errors, values)}
-                  isLoading={isSubmitting}
-                  onPress={() =>
-                    isDisabled(errors, values)
-                      ? {}
-                      : addMember(values, setFieldValue)
-                  }
-                  bg={
-                    isDisabled(errors, values) ? 'trueGray.900' : 'violet.600'
-                  }
-                  _pressed={{
-                    bg: 'violet.700'
-                  }}
-                  height={'100%'}
-                  width="1/5"
-                >
-                  <Plus color={isDisabled(errors, values) ? 'gray' : 'white'} />
-                </Button>
-              </HStack>
-              {errors.email && <Text color="red.500">{errors.email}</Text>}
-            </VStack>
-            <VStack>
-              <Box>
-                <Pressable
-                  style={{
-                    padding: 4
-                  }}
-                  onPress={() =>
-                    navigate('RecentMembers', {
-                      members: values.members,
-                      setMembers: emails => setFieldValue('members', emails)
-                    })
-                  }
-                >
-                  <HStack justifyContent="space-between" alignItems="center">
-                    <Text color="white" textTransform="uppercase">
-                      Ver membros recentes
-                    </Text>
-                    <ChevronRightIcon color="white" />
-                  </HStack>
-                </Pressable>
-              </Box>
-            </VStack>
-            <VStack space={4}>
-              <HStack space={2} alignItems="center">
-                <Text color="white" fontSize="xl">
-                  Membros:
+                <Text fontSize={28} color="white">
+                  Gostaria de já adicionar alguém no seu grupo?
                 </Text>
-                <Badge rounded="2xl">{values.members.length + 1}</Badge>
-              </HStack>
-              <MembersList
-                members={values.members.map(email => {
-                  return {
-                    email,
-                    endComponent: (
-                      <IconButton
-                        onPress={() =>
-                          setFieldValue(
-                            'members',
-                            values.members.filter(
-                              memberEmail => memberEmail !== email
+              </VStack>
+              <VStack>
+                <HStack space={2}>
+                  <Box width="4/5">
+                    <TextField
+                      error={
+                        touched.email && errors.email ? errors.email : undefined
+                      }
+                      hideMessageError
+                      onChangeText={handleChange('email')}
+                      onBlur={handleBlur('email')}
+                      placeholder="E-mail do membro"
+                      value={values.email || ''}
+                    />
+                  </Box>
+                  <Button
+                    disabled={isDisabled(errors, values)}
+                    isLoading={isSubmitting}
+                    onPress={() =>
+                      isDisabled(errors, values)
+                        ? {}
+                        : addMember(values, setFieldValue)
+                    }
+                    bg={
+                      isDisabled(errors, values) ? 'trueGray.900' : 'violet.600'
+                    }
+                    _pressed={{
+                      bg: 'violet.700'
+                    }}
+                    height={'100%'}
+                    width="1/5"
+                  >
+                    <Plus
+                      color={isDisabled(errors, values) ? 'gray' : 'white'}
+                    />
+                  </Button>
+                </HStack>
+                {errors.email && <Text color="red.500">{errors.email}</Text>}
+              </VStack>
+              <VStack>
+                <Box>
+                  <Pressable
+                    style={{
+                      padding: 4
+                    }}
+                    onPress={() =>
+                      navigate('RecentMembers', {
+                        members: values.members,
+                        setMembers: emails => setFieldValue('members', emails)
+                      })
+                    }
+                  >
+                    <HStack justifyContent="space-between" alignItems="center">
+                      <Text color="white" textTransform="uppercase">
+                        Ver membros recentes
+                      </Text>
+                      <ChevronRightIcon color="white" />
+                    </HStack>
+                  </Pressable>
+                </Box>
+              </VStack>
+              <VStack space={4}>
+                <HStack space={2} alignItems="center">
+                  <Text color="white" fontSize="xl">
+                    Membros:
+                  </Text>
+                  <Badge rounded="2xl">{values.members.length + 1}</Badge>
+                </HStack>
+                <MembersList
+                  members={values.members.map(email => {
+                    return {
+                      email,
+                      endComponent: (
+                        <IconButton
+                          onPress={() =>
+                            setFieldValue(
+                              'members',
+                              values.members.filter(
+                                memberEmail => memberEmail !== email
+                              )
                             )
-                          )
-                        }
-                        icon={({ size }) => <Trash color="white" size={size} />}
-                      />
-                    )
-                  }
-                })}
-                autoInclude
-                fetchUser
-              />
+                          }
+                          icon={({ size }) => (
+                            <Trash color="white" size={size} />
+                          )}
+                        />
+                      )
+                    }
+                  })}
+                  autoInclude
+                  fetchUser
+                />
+              </VStack>
             </VStack>
+            <Box mt={12}>
+              <SubmitButton
+                title="Criar grupo"
+                isSubmitting={isSubmitting}
+                handleSubmit={handleSubmit}
+              />
+            </Box>
           </VStack>
-          <Box my={8}>
-            <SubmitButton
-              isSubmitting={isSubmitting}
-              handleSubmit={handleSubmit}
-            />
-          </Box>
-        </VStack>
-      )}
-    </Formik>
+        )}
+      </Formik>
+    </ScrollView>
   )
 }
