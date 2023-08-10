1ef029c9e501125280e5bc72360f112d434cf6fc
Victor Aparecido Barbato Rossi <victorap93@gmail.com>
2023-08-10 19:58:21 -0300

    Change expanse button location and fix calendar date list.

--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -1,7 +1,9 @@
 import { Box, HStack, Text, VStack } from 'native-base'
-import React, { ReactNode } from 'react'
+import React, { ReactNode, useState } from 'react'
 import BackButton from './BackButton'
 import MenuButton from './MenuButton'
+import { TouchableOpacity } from 'react-native'
+import { CaretCircleDown, CaretCircleUp } from 'phosphor-react-native'
 
 interface BoxAppBarProps {
   children: ReactNode
@@ -30,28 +32,47 @@ export default function AppBar({
   right,
   bottom
 }: AppBarProps) {
+
+  const [openBottom, setOpenBottom] = useState(bottom ? true : false)
+
   return (
-    <BoxAppBar>
-      <VStack justifyContent="space-between" space={2}>
-        <HStack justifyContent="space-between" alignItems="center" px={4}>
-          <Box>
-            {left ? (
-              left === 'back' ? (
-                <BackButton onPress={onPress} />
+    <>
+      <BoxAppBar>
+        <VStack justifyContent="space-between" space={2}>
+          <HStack justifyContent="space-between" alignItems="center" px={4}>
+            <Box>
+              {left ? (
+                left === 'back' ? (
+                  <BackButton onPress={onPress} />
+                ) : (
+                  <MenuButton />
+                )
               ) : (
-                <MenuButton />
-              )
+                ''
+              )}
+            </Box>
+            <Text fontSize="lg" color="white">
+              {title}
+            </Text>
+            <Box>{right}</Box>
+          </HStack>
+          {openBottom && bottom}
+        </VStack>
+      </BoxAppBar>
+      {
+        bottom && <HStack
+          justifyContent="center"
+          marginTop={"-12.5px"}
+        >
+          <TouchableOpacity onPress={() => setOpenBottom(!openBottom)}>
+            {openBottom ? (
+              <CaretCircleUp weight="fill" color="white" />
             ) : (
-              ''
+              <CaretCircleDown weight="fill" color="white" />
             )}
-          </Box>
-          <Text fontSize="lg" color="white">
-            {title}
-          </Text>
-          <Box>{right}</Box>
+          </TouchableOpacity>
         </HStack>
-        {bottom}
-      </VStack>
-    </BoxAppBar>
+      }
+    </>
   )
 }
