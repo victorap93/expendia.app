d5a826bf4f2e99b36239db6dca1ae62de98bb702
Luigi Raynel <74196979+luigi-raynel-dev@users.noreply.github.com>
2023-09-12 22:32:39 -0300

    feat: display switch for accept required terms

--- src/screens/Register.tsx
+++ src/screens/Register.tsx
@@ -1,6 +1,15 @@
 import React from 'react'
-import { Alert } from 'react-native'
-import { Box, Center, Pressable, Text, VStack, useToast } from 'native-base'
+import { Alert, LogBox } from 'react-native'
+import {
+  Box,
+  Center,
+  HStack,
+  Pressable,
+  Switch,
+  Text,
+  VStack,
+  useToast
+} from 'native-base'
 import BackButton from '../components/BackButton'
 import { Formik } from 'formik'
 import * as Yup from 'yup'
@@ -13,13 +22,20 @@ import { useAuth } from '../hooks/useAuth'
 import { FormSignUp } from './SignUp'
 import SubmitButton from '../components/SubmitButton'
 
+LogBox.ignoreLogs([
+  'We can not support a function callback. See Github Issues for details https://github.com/adobe/react-spectrum/issues/2320'
+])
+
 export interface FormProfile {
   firstname: string
   lastname: string
   hasPassword?: boolean
 }
 
-interface FormRegister extends FormProfile, FormSignUp {}
+interface FormRegister extends FormProfile, FormSignUp {
+  acceptPrivacyPolicy: boolean
+  acceptTermsOfUse: boolean
+}
 
 export default function Register() {
   const route = useRoute()
@@ -57,7 +73,9 @@ export default function Register() {
           password,
           confirmPassword,
           firstname: '',
-          lastname: ''
+          lastname: '',
+          acceptPrivacyPolicy: false,
+          acceptTermsOfUse: false
         } as FormRegister
       }
       validationSchema={Yup.object({
@@ -65,7 +83,13 @@ export default function Register() {
         password: Yup.string().required(),
         confirmPassword: Yup.string().required(),
         firstname: Yup.string().required('Digite seu primeiro nome.'),
-        lastname: Yup.string().required('Digite seu sobrenome.')
+        lastname: Yup.string().required('Digite seu sobrenome.'),
+        acceptPrivacyPolicy: Yup.boolean().isTrue(
+          'Para criar sua conta é necessário estar de acordo com nossa política de privacidade.'
+        ),
+        acceptTermsOfUse: Yup.boolean().isTrue(
+          'Para criar sua conta é necessário estar de acordo com nossos termos de uso.'
+        )
       })}
       onSubmit={(values, { setSubmitting }) => submit(values, setSubmitting)}
     >
@@ -75,7 +99,9 @@ export default function Register() {
         handleSubmit,
         values,
         errors,
-        isSubmitting
+        isSubmitting,
+        submitCount,
+        setFieldValue
       }) => (
         <VStack flex={1} space={2} px={4} py={8} justifyContent="space-between">
           <VStack>
@@ -87,14 +113,22 @@ export default function Register() {
             </Text>
             <VStack space={6}>
               <TextField
-                error={errors.firstname}
+                error={
+                  submitCount > 0 && errors.firstname
+                    ? errors.firstname
+                    : undefined
+                }
                 onChangeText={handleChange('firstname')}
                 onBlur={handleBlur('firstname')}
                 value={values.firstname || ''}
                 placeholder="Digite seu primeiro nome..."
               />
               <TextField
-                error={errors.lastname}
+                error={
+                  submitCount > 0 && errors.lastname
+                    ? errors.lastname
+                    : undefined
+                }
                 onChangeText={handleChange('lastname')}
                 onBlur={handleBlur('lastname')}
                 value={values.lastname || ''}
@@ -111,6 +145,54 @@ export default function Register() {
                     .
                   </Text>
                 </Pressable>
+                <VStack space={1} my={2}>
+                  <HStack space={1} alignItems="center">
+                    <Switch
+                      isChecked={values.acceptPrivacyPolicy}
+                      onChange={() =>
+                        setFieldValue(
+                          'acceptPrivacyPolicy',
+                          !values.acceptPrivacyPolicy
+                        )
+                      }
+                      colorScheme="success"
+                    />
+                    <Text color="white" fontSize="md">
+                      Li e concordo com a{' '}
+                      <Text color="white" bold underline fontSize="md">
+                        política de privacidade.
+                      </Text>
+                    </Text>
+                  </HStack>
+                  <HStack space={1} alignItems="center">
+                    <Switch
+                      defaultIsChecked={values.acceptTermsOfUse}
+                      onChange={() =>
+                        setFieldValue(
+                          'acceptTermsOfUse',
+                          !values.acceptTermsOfUse
+                        )
+                      }
+                      colorScheme="success"
+                    />
+                    <Text color="white" fontSize="md">
+                      Li e concordo com os{' '}
+                      <Text color="white" bold underline fontSize="md">
+                        termos de uso.
+                      </Text>
+                    </Text>
+                  </HStack>
+                  {submitCount > 0 && errors.acceptPrivacyPolicy && (
+                    <Text color="red.500" fontSize="sm">
+                      {errors.acceptPrivacyPolicy}
+                    </Text>
+                  )}
+                  {submitCount > 0 && errors.acceptTermsOfUse && (
+                    <Text color="red.500" fontSize="sm">
+                      {errors.acceptTermsOfUse}
+                    </Text>
+                  )}
+                </VStack>
               </Center>
             </VStack>
           </VStack>
