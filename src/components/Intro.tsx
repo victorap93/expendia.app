83a911267d06624f12bf38c10e4c54d6d1b06dbf
Victor Aparecido Barbato Rossi <victorap93@gmail.com>
2024-01-10 08:39:11 -0300

    Ajuste no uso dos incones e aumento no tamanho dos botões de ação.

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -1,18 +1,20 @@
 import React from 'react'
 import AppIntroSlider from 'react-native-app-intro-slider'
-import { Icon, Text, VStack, View } from 'native-base'
+import { Text, VStack, View } from 'native-base'
 import { StyleSheet } from 'react-native'
 import { InterfaceVStackProps } from 'native-base/lib/typescript/components/primitives/Stack/VStack'
+import Icon from '@expo/vector-icons/MaterialCommunityIcons'
 
 const styles = StyleSheet.create({
   buttonCircle: {
     width: 'auto',
-    height: 40,
-    paddingHorizontal: 20,
+    height: 60,
+    paddingHorizontal: 25,
     backgroundColor: 'rgba(0, 0, 0, .2)',
-    borderRadius: 20,
+    borderRadius: 30,
     justifyContent: 'center',
     alignItems: 'center',
+    marginTop: -10
   }
 })
 
@@ -30,25 +32,32 @@ export default function Intro({ data, onDone }: IntroProps) {
       skipLabel="Pular"
       showSkipButton
       showPrevButton
-      dotStyle={{ backgroundColor: 'rgba(0, 0, 0, .2)' }}
+      dotStyle={{ 
+        backgroundColor: 'rgba(0, 0, 0, .2)',
+        marginTop: -10
+      }}
+      activeDotStyle={{
+        backgroundColor: 'rgba(255, 255, 255, 1)',
+        marginTop: -10
+      }}
       renderPrevButton={() => 
         <View style={styles.buttonCircle}>
-          <Icon name="arrow-left" size={24} color="white" />
+          <Icon name="arrow-left" size={30} color="white" />
         </View>
       } 
       renderNextButton={() => 
         <View style={styles.buttonCircle}>
-          <Icon name="arrow-right" size={24} color="white" />
+          <Icon name="arrow-right" size={30} color="white" />
         </View>
       }     
       renderDoneButton={() => 
         <View style={styles.buttonCircle}>
-          <Icon name="check" size={24} color="white" />
+          <Icon name="check" size={30} color="white" />
         </View>
       } 
       renderSkipButton={() =>
         <View style={styles.buttonCircle}>
-          <Text color="white">Pular</Text>
+          <Text fontSize={20} color="white">Pular</Text>
         </View>
       }
       renderItem={({ item, index }) => 
